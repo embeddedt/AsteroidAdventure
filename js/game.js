f1ebb4faef037f11c50470b3e8c8c91aeaf848f2
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-08-11 10:53:38 -0400

    Block entering non-numeric characters

--- js/game.js
+++ js/game.js
@@ -493,6 +493,12 @@ function hitBird(player, target) {
                 autocapitalize: 'off'
             },
             showCancelButton: false,
+            onOpen: function() {
+                const input = Swal.getInput()
+                input.oninput = () => {
+                    event.target.value = event.target.value.replace(/[^0-9+]/g, '');
+                };
+            },
             preConfirm: function(val) {
                 if(typeof val == 'undefined' || val.trim().length == 0) {
                     Swal.showValidationMessage("Your answer cannot be blank.");
